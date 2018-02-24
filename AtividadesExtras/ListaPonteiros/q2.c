5ff522fe2eb3623de4bebf18bef42c5e4b777df2
Rafaela Fernandes <rafaela.sjfernandes@gmail.com>
2018-02-24 09:02:02 -0300

    questao concluida

--- AtividadesExtras/ListaPonteiros/q2.c
+++ AtividadesExtras/ListaPonteiros/q2.c
@@ -1,7 +1,22 @@
+#include<stdio.h>
 
+void troca();
 
 int main(){
     
- int i;    
-    
+    int x, y;
+    printf("Informe o valor de x: ");
+    scanf("%d", &x);    
+    printf("Informe o valor de y: ");
+    scanf("%d", &y);
+    troca(&x, &y);
 } 
+
+void troca (int *p, int *q){
+    
+    int x;
+    x = *p;
+    *p = *q;
+    *q = x;
+    printf("x = %d - y = %d\n", *p, *q);
+}
